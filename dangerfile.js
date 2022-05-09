41650d4d5b8848f7e6bf82f02f4353123d6a9ad2
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2022-05-09 14:57:33 +0900

    fix: add js file

---
+++ dangerfile.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-console */
+const dangerObj = require('danger');
+const danger = dangerObj.danger;
+const warn = dangerObj.warn;
+const marked = require('marked');
+const jsdom = require('jsdom');
+const JSDOM = jsdom.JSDOM;
+
+const checkExistTitle = (html) => {
+  if (html.indexOf('Proposed Changes') == -1) {
+    warn('Proposed Changesがありません。追加してください。')
+  }
+
+  if (html.indexOf('Implementation') == -1) {
+    warn('Implementationがありません。追加してください。')
+  }
+
+  if (html.indexOf('Task list to merge this PR') == -1) {
+    warn('Task list to merge this PRがありません。追加してください。')
+  }
+}
+
+const checkTextContext = (dom) => {
+  const ulList = dom.window.document.querySelectorAll('ul')
+
+  if (ulList.length < 3) {
+    warn('Proposed Change、Implementation、Task list to merge this PRが適切に入力されていません')
+  }
+}
+
+const checkCheckboxList = (dom) => {
+  const inputList = dom.window.document.querySelectorAll('input')
+
+  let checkedFlag = true
+  for (let index = 0; index < inputList.length; index++) {
+    const inputElm = inputList[index]
+    if (inputElm.type === 'checkbox' && !inputElm.checked) {
+      checkedFlag = false
+    }
+  }
+
+  if (!checkedFlag) {
+    warn('チェックされていないタスクがあります。')
+  }
+}
+
+const main = () => {
+  try {
+    const body = danger.github.pr.body
+    const html = marked(body)
+    const dom = new JSDOM(html)
+
+    checkExistTitle(html)
+
+    checkTextContext(dom)
+
+    checkCheckboxList(dom)
+  } catch (e) {
+    console.error(e)
+    warn('PRテンプレートが正しく使用されていません。最新の状態であるか確認してください')
+  }
+}
+
+main()
